e9d17bdfe7842165b249989252cd9ce3acfd1afe
vavadimir <freesoulvova@gmail.com>
2017-10-22 14:09:22 +0300

    Add limitations and UNIQUE constraints for models.

--- cinema/models.py
+++ cinema/models.py
@@ -3,38 +3,48 @@ from django.conf import settings
 
 
 class Film(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000)
-    genre = models.CharField(max_length=20)
-    premiere_date = models.DateField()
-    session_time = models.TimeField()
-    film_duration = models.IntegerField()
-    price = models.FloatField()
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField(max_length=1000, null=False)
+    genre = models.CharField(max_length=20, null=False)
+    premiere_date = models.DateField(null=False)
+    session_time = models.TimeField(null=False)
+    film_duration = models.IntegerField(null=False)
+    price = models.FloatField(null=False)
 
     def __str__(self):
         return str(self.id)
 
+    class Meta:
+        unique_together = ('title', 'session_time')
+
 
 class Poster(models.Model):
-    film = models.ForeignKey(Film)
-    pic = models.ImageField(upload_to='images/')
+    film = models.ForeignKey(Film, null=False)
+    pic = models.ImageField(upload_to='images/', null=False)
     is_main = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.pic)
 
+    class Meta:
+        unique_together = ('film', 'pic')
+
 
 class BookedPlace(models.Model):
-    film = models.ForeignKey(Film, related_name="tickets")
+    film = models.ForeignKey(Film, related_name="tickets", null=False)
     customer = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         db_column="customer",
         related_name="tickets",
+        null=False
     )
-    place = models.IntegerField()
-    row = models.IntegerField()
+    place = models.IntegerField(null=False)
+    row = models.IntegerField(null=False)
     booking_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '%d %d' % (self.row, self.place)
+
+    class Meta:
+        unique_together = ('film', 'place', 'row')
